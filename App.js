d830e78be0fe1a99abb1e75320970d5b481de3e0
Abrar Sardar <abrarsardar8955@gmail.com>
2023-06-04 16:46:56 +0500

    Firestore Practice

---
+++ App.js
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+import Navigation from "./src/Navigation";
+
+export default function App() {
+  return (
+    <Navigation />
+  //  <View>
+  //   <Text>APP.js</Text>
+  //  </View>
+  );
+}
